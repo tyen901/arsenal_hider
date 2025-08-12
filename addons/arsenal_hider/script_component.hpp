92dd4cd1020fdd923f1fa707b6020605e53f0e87
Tom Campbell <tyen901@gmail.com>
2025-08-12 18:18:57 +1200

    Update script_component.hpp

--- addons/arsenal_hider/script_component.hpp
+++ addons/arsenal_hider/script_component.hpp
@@ -1,13 +1,15 @@
+#define PREFIX ace
 #define COMPONENT arsenal_hider
-#define COMPONENT_BEAUTIFIED Arsenal Hider
-#include "\z\ace\addons\main\script_mod.hpp"
 
-#ifdef DEBUG_ENABLED_ARSENAL_HIDER
-    #define DEBUG_MODE_FULL
+#include "\x\cba\addons\main\script_macros_common.hpp"
+
+#ifdef DISABLE_COMPILE_CACHE
+    #undef PREP
+    #define PREP(var1) TRIPLES(ADDON,fnc,var1) = compile preProcessFileLineNumbers QUOTE(PATHTOF(functions\DOUBLES(fnc,var1).sqf))
+#else
+    #undef PREP
+    #define PREP(var1) [QUOTE(PATHTOF(functions\DOUBLES(fnc,var1).sqf)), QUOTE(TRIPLES(ADDON,fnc,var1))] call cba_fnc_compileFunction
 #endif
 
-#ifdef DEBUG_SETTINGS_ARSENAL_HIDER
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_ARSENAL_HIDER
-#endif
-
-#include "\z\ace\addons\main\script_macros.hpp"
+#define PREP_RECOMPILE_START    if (isNil 'DOUBLES(PREFIX,ADDON)') then { DOUBLES(PREFIX,ADDON) = false; }; if (DOUBLES(PREFIX,ADDON)) then {
+#define PREP_RECOMPILE_END      };
